3a23c85eb1c6dc9079119a0f0530a505e8dcb556
Nicholas <aganirabn@gmail.com>
2023-02-26 22:22:27 -0800

    created deploy script

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
 <template>
   <div id="header">
     <h1 id="intro"> Hi, I'm Nick! </h1>
-    <img id="photo" src="picture.jpg" alt="photo" />
+    <img id="photo" src="/picture.jpg" alt="photo" />
     <h1 id="welcome"> Welcome To My Portfolio </h1>
   </div>
 </template>
